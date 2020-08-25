45c15d45676358d8a57052545c486e7505c01aff
xiaocheng24k <xiaocheng_yy@163.com>
2020-08-25 15:40:33 +0800

    引入redis

--- spp-dubbo-provider/src/main/java/cn/com/xiaocheng/api/impl/TestApiImpl.java
+++ spp-dubbo-provider/src/main/java/cn/com/xiaocheng/api/impl/TestApiImpl.java
@@ -4,6 +4,7 @@ import cn.com.xiaocheng.api.TestApi;
 //import cn.com.xiaocheng.util.RedisUtil;
 import cn.com.xiaocheng.pojo.User;
 import cn.com.xiaocheng.dao.TestDao;
+import cn.com.xiaocheng.util.RedisUtil;
 import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +18,15 @@ public class TestApiImpl implements TestApi {
 
     private static final Logger logger = LoggerFactory.getLogger(TestApiImpl.class);
 
-//    @Autowired
-//    RedisUtil redisUtil;
+    @Autowired
+    RedisUtil redisUtil;
     @Autowired
     TestDao testDao;
 
     public String testservice() {
         logger.info("改方法被调用");
-//        boolean set = redisUtil.set("aaa", "bbb");
-//        System.out.println(set);
+        boolean set = redisUtil.set("aaa", "bbb");
+        logger.info("redis调用结果: {}",set);
         List<User> users = testDao.selectUser();
         logger.info("查询数据库为 : {}",JSON.toJSONString(users));
         return "hello";
